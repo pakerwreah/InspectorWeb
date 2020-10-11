3f61ed3ad3ba803cf48142861d0762df1e5b68fd
Carlos César Neves Enumo <paker_wreah@hotmail.com>
2020-10-11 17:28:01 -0300

    Fix update check

--- src/App.vue
+++ src/App.vue
@@ -218,7 +218,7 @@
             })
         },
         methods: {
-            open,
+            open: open.bind(window),
             ticker () {
                 this.now = Date.now()
             },
