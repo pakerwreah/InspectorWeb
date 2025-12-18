dd3a46b577fd700ad97feef6f1a417bb85b5f0ba
Carlos Enumo <paker_wreah@hotmail.com>
2025-12-17 21:31:31 -0300

    Fix gh-pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import vuetify from 'vite-plugin-vuetify'
 
 export default defineConfig({
+    base: './',
     plugins: [vue(), vueDevTools(), vuetify()],
     resolve: {
         alias: {
